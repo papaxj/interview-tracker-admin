246198e8c5a1223c9d9a0c90bce976443fd8a156
tongda <124402926@qq.com>
2026-06-13 12:19:32 +0800

    fix(identity): 增加姓名列宽

--- src/views/identity/index.vue
+++ src/views/identity/index.vue
@@ -113,7 +113,7 @@ onMounted(async () => {
       stripe
     >
       <el-table-column type="index" label="#" width="50" />
-      <el-table-column prop="name" label="姓名" width="100" />
+      <el-table-column prop="name" label="姓名" width="200" />
       <el-table-column prop="idCard" label="身份证号" width="180" />
       <el-table-column prop="gender" label="性别" width="80">
         <template #default="{ row }">
